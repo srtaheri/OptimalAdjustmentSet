14505d09f753e85ad7a59de6fe6aac886b7f4a8b
srtaheri <srtaheri66@gmail.com>
2022-12-16 13:43:44 -0500

    added functions to find all the valid adj set

--- main_functions.R
+++ main_functions.R
@@ -39,11 +39,15 @@ foreach::getDoParWorkers()
 ## num_dp : number of data points from the synthetic_data used to estimate the query and the variance. Default is 100.
 find_query_est_for_given_adj_set <- function(exposure,  exposure_intv_value = 0, outcome, query = "ATE", valid_adj_set, method = "lm", synthetic_data, num_dp = 100, num_synthetic_data_sets = 100) {
   adjSet = valid_adj_set
+  if(length(adjSet) < 1) {
+    method = "lm"
+  }
   data_nds = synthetic_data[seq(1:num_synthetic_data_sets)]
   set.seed(20)
   data = lapply(data_nds, function(x) x[sample(1:nrow(x),num_dp),])
   
   if (method == "lm") {
+    print("lm")
     formula = paste(outcome,
                     paste(c(exposure,adjSet), collapse = " + "),
                     sep = " ~ ")
@@ -65,7 +69,6 @@ find_query_est_for_given_adj_set <- function(exposure,  exposure_intv_value = 0,
   }
   if (method == "AIPW") {
     if(query == "ATE") {
-      start_time = Sys.time() #
       estimate_query = unlist(lapply(data, function(x) {
         AIPW_SL <- AIPW$new(Y = x[,outcome],
                             A = x[,exposure],
@@ -79,8 +82,6 @@ find_query_est_for_given_adj_set <- function(exposure,  exposure_intv_value = 0,
         })
         return(AIPW_SL$result["ATC Risk Difference","Estimate"])
       }))
-      end_time <- Sys.time() #
-      end_time - start_time #
     }
   }
   
@@ -92,13 +93,13 @@ find_query_est_for_given_adj_set <- function(exposure,  exposure_intv_value = 0,
       
       interv0 <- x # 2nd copy: treatment set to 0, outcome to missing
       interv0$interv <- 0
-      interv0$EGFR <- 0
-      interv0$cytok <- NA
+      interv0[,exposure] <- 0
+      interv0[,outcome] <- NA
       
       interv1 <- x # 3rd copy: treatment set to 1, outcome to missing
       interv1$interv <- 1
-      interv1$EGFR <- 1
-      interv1$cytok <- NA
+      interv1[,exposure] <- 1
+      interv1[,outcome] <- NA
       
       onesample <- rbind(x, interv0, interv1) # combining datasets
       # This formula can change. It can be quadratic
@@ -125,8 +126,7 @@ find_query_est_for_given_adj_set <- function(exposure,  exposure_intv_value = 0,
                                      data = x, 
                                      estimand = query,  # Find the ATE
                                      method = "ps")  # Build weights with propensity scores
-        data_weightit <- x %>% 
-          mutate(ipw = weights_weightit$weights)
+        data_weightit <- mutate(x, ipw = weights_weightit$weights)
         formula2 = paste(outcome,
                          paste(exposure, collapse = " + "),
                          sep = " ~ ")
@@ -183,90 +183,191 @@ find_ranked_var_and_query_est_for_all_valid_adj_sets = function(g, exposure, exp
   output = list("sorted_adj_set_based_on_var" = sorted_var, "sorted_query_est" = sorted_query_est)
   return(output)
 }
+# Get all the forbidden variables for the adjustment set
+get_forbidden_vars <- function(g, from, to) {
+  proper_paths <- dagitty::paths(x = g, from = from, to = to, directed = TRUE)
+  vars = c()
+  for (path in proper_paths$paths) {
+    vars = c(vars, unlist(strsplit(path, split = " -> ")))
+  }
+  vars = unique(vars)
+  forbidden_vars = vars
+  for (var in vars) {
+    forbidden_vars = c(forbidden_vars, descendants(x = g, v = var))
+  }
+  forbidden_vars = unique(forbidden_vars)
+  return(forbidden_vars)
+}
 
 # Create all valid adjustment sets excluding latent variables
 ## g: A DAG or ADMG in the dagitty format
 ## exposure : The exposure/cause/treatment variable (target of intervention)
 ## outcome : The effect/outcome variable
+
+# covariates <- setdiff(names(x), c(exposure, outcome))
+# subsets <- (expand.grid(rep(list(0:1), length(covariates))))
+# r <- lapply(1:nrow(subsets), function(i) {
+#   Z <- covariates[as.logical(subsets[i, ])]
+#   if (isAdjustmentSet(x, Z, exposure = exposure, outcome = outcome)) {
+#     Z
+#   }
+#   else {
+#     NA
+#   }
+# })
+# non.r <- which(sapply(r, function(x) isTRUE(is.na(x))))
+# if (length(non.r) > 0) {
+#   r <- r[-non.r]
+# }
+# r <- structure(r, class = "dagitty.sets")
+
+# all_valid_adj_sets <- function(g, exposure, outcome) {
+#   
+#   adj_minimal <- adjustmentSets(x = g, exposure = exposure, outcome = outcome, type = "minimal")
+#   forbidden_vars <- get_forbidden_vars(g = g, from = exposure, to = outcome)
+#   covariates <- setdiff(names(g), forbidden_vars)
+#   #adj_canonical <- adjustmentSets(x = g, exposure = exposure, outcome = outcome, type = "canonical")
+#   
+#   result <- list()
+#   for (i in 1:length(adj_minimal)) {
+#     difference <- setdiff(covariates, adj_minimal[[i]])
+#     if(i>1 && sum(difference %in% adj_minimal[[i-1]]) > 0) {
+#       difference <- difference[-which(difference %in% adj_minimal[[1]])]
+#     }
+#     my_combi <- unlist(lapply(1:length(difference),    # Get all combinations
+#                               combinat::combn, 
+#                               x = difference,
+#                               simplify = FALSE), 
+#                               recursive = FALSE)
+#     my_combi_vars = lapply(my_combi,function(x) sort(c(adj_minimal[[i]],x)))
+#     r = c()
+#     for (my_combi_var_idx in 1:length(my_combi_vars)) {
+#       if(isAdjustmentSet(x = g, Z = my_combi_vars[[my_combi_var_idx]], exposure = exposure, outcome = outcome) == FALSE) {
+#         r = c(r,my_combi_var_idx)
+#       }
+#     }
+#     if (length(r) > 0) {
+#       my_combi_vars <- my_combi_vars[-r]
+#     }
+#     my_combi_vars[[length(my_combi_vars)+1]] <- adj_minimal[[i]]
+#     result[[i]] <- my_combi_vars
+#   }
+#   result = unlist(result, recursive = F)
+#   r = c()
+#   for (result_element_idx1 in 1:(length(result)-1)) {
+#     for (result_element_idx2 in (result_element_idx1 + 1):length(result)) {
+#       if(length(result[[result_element_idx1]]) == length(result[[result_element_idx2]]) && sum(result[[result_element_idx1]] %in% result[[result_element_idx2]]) == length(result[[result_element_idx1]])) {
+#         r = c(r,result_element_idx1)
+#       }
+#     }
+#   }
+#   
+#   return(result)
+# }
 all_valid_adj_sets <- function(g, exposure, outcome) {
   
-  adj_minimal <- adjustmentSets(x = g, exposure = exposure, outcome = outcome, type = "minimal")
-  adj_canonical <- adjustmentSets(x = g, exposure = exposure, outcome = outcome, type = "canonical")
+  #adj_minimal <- adjustmentSets(x = g, exposure = exposure, outcome = outcome, type = "minimal")
+  forbidden_vars <- get_forbidden_vars(g = g, from = exposure, to = outcome)
+  covariates <- setdiff(names(g), forbidden_vars)
+  #adj_canonical <- adjustmentSets(x = g, exposure = exposure, outcome = outcome, type = "canonical")
+  my_combi <- unlist(lapply(1:length(covariates),    # Get all combinations
+                            combinat::combn, 
+                            x = covariates,
+                            simplify = FALSE), 
+                     recursive = FALSE)
   
-  result <- list()
-  for (i in 1:length(adj_minimal)) {
-    difference <- setdiff(adj_canonical[[1]], adj_minimal[[i]])
-    if(i>1 && sum(difference %in% adj_minimal[[i-1]]) > 0) {
-      difference <- difference[-which(difference %in% adj_minimal[[1]])]
+  r = c()
+  for (my_combi_var_idx in 1:length(my_combi)) {
+    if(isAdjustmentSet(x = g, Z = my_combi[[my_combi_var_idx]], exposure = exposure, outcome = outcome) == FALSE) {
+      r = c(r,my_combi_var_idx)
     }
-    my_combi <- unlist(lapply(1:length(difference),    # Get all combinations
-                              combinat::combn, 
-                              x = difference,
-                              simplify = FALSE), 
-                              recursive = FALSE)
-    my_combi_vars = lapply(my_combi,function(x) c(adj_minimal[[i]],x))
-    my_combi_vars[[length(my_combi_vars)+1]] <- adj_minimal[[i]]
-    result[[i]] <- my_combi_vars
   }
-  return(result)
+  if (length(r) > 0) {
+    my_combi <- my_combi[-r]
+  }
+  
+  return(my_combi)
 }
-
-#Simplify the graph
+#Simplifies the graph according to Evan's simplification rules.
 #The simplification rules are as follows:
 #1) Remove latent variable with no children from the graph
 #2) Remove an exogenous latent variable that has at most one child
 #3) Transform a latent variable with parents to an exogenous variable where all its parents are connected to its children
 #4) If U and W are latent variables where children of W are a subset of children of U, then, W can be removed.
-
-g <- dagitty( "dag { U1 [latent]
-                            U2 [latent]
-                            U3 [latent]
-                            U4 [latent]
-                            U5 [latent]
-                            Z1 -> Z2
-                            Z2 -> Z3
-                            Z3 -> Y
-                            Z1 -> X
-                            X -> Y;
-                            Z4 -> Z3
-                            Z4 -> Y
-                            X -> U1
-                            U2 -> Z1
-                            U2 -> Z2
-                            U3 -> Z1
-                            U3 -> Z2
-                            U3 -> Z3
-                            U5 -> Z3
-                            U5 -> Y
-                            U4 -> Y
-                            Z4 -> U5
-                            Z4 -> U5
-                            Z5 ->U5}" )
-g_string <- " U1 [latent]
-              U2 [latent]
-              U3 [latent]
-              U4 [latent]
-              U5 [latent]
-              Z1 -> Z2
-              Z2 -> Z3
-              Z3 -> Y
-              Z1 -> X
-              X -> Y
-              Z4 -> Z3
-              Z4 -> Y
-              X -> U1
-              U2 -> Z1
-              U2 -> Z2
-              U3 -> Z1
-              U3 -> Z2
-              U3 -> Z3
-              U5 -> Z3
-              U5 -> Y
-              U4 -> Y
-              Z4 -> U5
-              Z5 ->U5"
-
-generate_simplified_graph <- function(g, g_string) {
+# add this too: remove all descendants of the effect, if effect is specified
+#Input:
+#g: A dagitty graph that contains latent nodes and does not have any bidirected edges
+#g_string: The string format of the g
+#Output:
+# A dagitty format string of a simpified dag.
+#example input:
+# g <- dagitty( "dag {U1 [latent]
+#                     U2 [latent]
+#                     U3 [latent]
+#                     U4 [latent]
+#                     U5 [latent]
+#                     Z1 -> Z2
+#                     Z2 -> Z3
+#                     Z3 -> Y
+#                     Z1 -> X
+#                     X -> Y;
+#                     Z4 -> Z3
+#                     Z4 -> Y
+#                     X -> U1
+#                     U2 -> Z1
+#                     U2 -> Z2
+#                     U3 -> Z1
+#                     U3 -> Z2
+#                     U3 -> Z3
+#                     U5 -> Z3
+#                     U5 -> Y
+#                     U4 -> Y
+#                     Z4 -> U5
+#                     Z4 -> U5
+#                     Z5 ->U5}")
+# g_string <- " U1 [latent]
+#               U2 [latent]
+#               U3 [latent]
+#               U4 [latent]
+#               U5 [latent]
+#               Z1 -> Z2
+#               Z2 -> Z3
+#               Z3 -> Y
+#               Z1 -> X
+#               X -> Y
+#               Z4 -> Z3
+#               Z4 -> Y
+#               X -> U1
+#               U2 -> Z1
+#               U2 -> Z2
+#               U3 -> Z1
+#               U3 -> Z2
+#               U3 -> Z3
+#               U5 -> Z3
+#               U5 -> Y
+#               U4 -> Y
+#               Z4 -> U5
+#               Z5 ->U5"
+#Example output:
+# g_string <- " U3 [latent]
+#               U5 [latent]
+#               Z1 -> Z2
+#               Z2 -> Z3
+#               Z3 -> Y
+#               Z1 -> X
+#               X -> Y
+#               Z4 -> Z3
+#               Z4 -> Y
+#               U3 -> Z1
+#               U3 -> Z2
+#               U3 -> Z3
+#               U5 -> Z3
+#               U5 -> Y
+#               Z5 -> Z3
+#               Z5 -> Y
+#               Z4 -> Z3
+#               Z4 -> Y"
+generate_simplified_graph_string <- function(g, g_string) {
   glines = lapply(strsplit(g_string, "\n"), trimws)
   #find the latent nodes
   e_from = c()
@@ -323,9 +424,78 @@ generate_simplified_graph <- function(g, g_string) {
       }
   }
   }  
-  edge_df = edge_df[-remove_rows,]
-  edge_df = rbind(edge_df, add_edge_df)
+  if(length(remove_rows) > 0) {
+    edge_df = edge_df[-remove_rows,]
+  }
   rownames(edge_df) = seq(1:nrow(edge_df))
-  
+  remaining_lt_nodes = c()
+  for (lt in latent_nodes) {
+    if(length(which(edge_df["from"] == lt) > 0)) {
+      remaining_lt_nodes = c(remaining_lt_nodes,lt)
+    }
+  }
+  edge_df_str = paste(apply(edge_df, 1, function(x) paste0(x[1],"->",x[2])), collapse=";\n")
+  latent_nodes_string_str = paste(remaining_lt_nodes, " [latent]" , collapse=";\n")
+  dagitty_input_str = paste("dag {", "\n" , latent_nodes_string_str, edge_df_str, ";\n}", sep="")
+  return(dagitty_input_str)
+}
 
+#removes latent variables and show their presence by bidirected edges. Outputs a dagitty string
+#Inputs:
+#g_string: A dagitty format string of the dag that contains latent nodes and no bidirected edges
+#Output: A dagitty format string of an admg that doesn't contain any latent nodes and has bidirected edges.
+#Example input:
+# g_string = "Z3 [latent]
+#             Z1 -> Z2
+#             Z3 -> Y
+#             Z3 -> Z2
+#             Z1 -> X
+#             X -> Y
+#             Z4 -> X"
+#Example output:
+#            "Z1 -> Z2
+#             Z2 <-> Y
+#             Z1 -> X
+#             X -> Y
+#             Z4 -> X"
+generate_dagitty_input_string <- function(g_string) {
+  glines = lapply(strsplit(g_string, "\n"), trimws)
+  
+  e_from = c()
+  e_to = c()
+  latent_nodes = c()
+  for (line in glines[[1]]) {
+    if (grepl("->", line)) {
+      edge_splitted = lapply(strsplit(line, "->"), trimws)
+      e_from = c(e_from, edge_splitted[[1]][1])
+      e_to = c(e_to, edge_splitted[[1]][2])
+    }
+    if (grepl("latent", line)) {
+      latent_nodes = c(latent_nodes, strsplit(line, " ")[[1]][1])
+    }
+  }
+  
+  reg_edge_df = data.frame("from"=e_from, "to"=e_to)
+  
+  lt_edge_df = data.frame(matrix(nrow = 0, ncol = 3))
+  colnames(lt_edge_df) = c("from", "to")
+  
+  for (lt in latent_nodes) {
+    lt_indices = which(reg_edge_df[,"from"] == lt)
+    lt_edges = reg_edge_df[lt_indices,]
+    lt_children = lt_edges[,"to"]
+    new_edges = t(as.data.frame(combn(lt_children, 2)))
+    colnames(new_edges) = c("from", "to")
+    lt_edge_df = rbind(lt_edge_df, new_edges)
+    reg_edge_df = reg_edge_df[-lt_indices,]
+  }
+  rownames(reg_edge_df) = seq(1:nrow(reg_edge_df))
+  rownames(lt_edge_df) = seq(1:nrow(lt_edge_df))
+  
+  lt_edges_str = paste(apply(lt_edge_df, 1, function(x) paste0(x[1],"<->",x[2])), collapse=";\n")
+  
+  reg_edges_str = paste(apply(reg_edge_df, 1, function(x) paste0(x[1],"->",x[2])), collapse = ";\n")
+  
+  dagitty_input_str = paste("dag {", reg_edges_str, ";\n", lt_edges_str, ";\n}", sep="")
+  return(dagitty_input_str)
 }
