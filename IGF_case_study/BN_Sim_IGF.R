c0c88c07e2390e1f8315c8201770c614e0219b87
srtaheri <srtaheri66@gmail.com>
2023-01-06 15:47:22 -0500

    modified BN sim

--- IGF_case_study/BN_Sim_IGF.R
+++ IGF_case_study/BN_Sim_IGF.R
@@ -2,14 +2,14 @@ library(tidyverse)
 
 #IGFdata = readRDS("data/observational_igf.RData")
 
-origdata = read_csv("data/obs_data.csv")
+origdata = read_csv("real_data/obs_data.csv")
 
 origdata$U = rnorm(nrow(origdata))
 
 
 source("../ContinuousPGM.R")
 
-ggplot(sampledata,aes(x=PI3K,y=AKT)) + geom_point() + geom_smooth(method="gam")
+ggplot(origdata,aes(x=PI3K,y=AKT)) + geom_point() + geom_smooth(method="gam")
 
 fig_5_DAG = c("U~1",
               "SOS~U",
@@ -27,7 +27,8 @@ fig_5_methods[[5]] = "gam"
 
 ## Train and Simulate function 
 
-train_and_simulate = function(data,n_train=nrow(data),n_sim=n_train,n_datasets=1000){
+train_and_simulate = function(data,n_train=nrow(data),n_sim=n_train,n_datasets=1000, seed = 10){
+  set.seed(seed)
   n = nrow(data)
   stopifnot(n_train <= n)
   
@@ -53,21 +54,27 @@ train_and_simulate = function(data,n_train=nrow(data),n_sim=n_train,n_datasets=1
 ## Training and Simulating Data 
 
 start = proc.time()
-simdata_full = train_and_simulate(data=origdata)
+simdata_full = train_and_simulate(data=origdata, n_train = 1000, seed = 40)
 end = proc.time()
 print(end-start) #took 15 secs 
 
-simdata_500 = train_and_simulate(data=origdata,n_train = 500)
+simdata_500 = train_and_simulate(data=origdata,n_train = 500, seed = 40)
 
-simdata_100 = train_and_simulate(data=origdata,n_train = 100)
+simdata_100 = train_and_simulate(data=origdata,n_train = 100, seed = 40)
 
-simdata_20 = train_and_simulate(data=origdata,n_train = 20)
+simdata_50 = train_and_simulate(data=origdata,n_train = 50, seed = 40)
+
+simdata_30 = train_and_simulate(data=origdata,n_train = 30, seed = 40)
+
+simdata_20 = train_and_simulate(data=origdata,n_train = 20, seed = 40)
 
 #Saving
 
 saveRDS(simdata_full,"BN_simdata/IGF_simdata_1000.rdata")
 saveRDS(simdata_500,"BN_simdata/IGF_simdata_500.rdata")
 saveRDS(simdata_100,"BN_simdata/IGF_simdata_100.rdata")
+saveRDS(simdata_50,"BN_simdata/IGF_simdata_50.rdata")
+saveRDS(simdata_30,"BN_simdata/IGF_simdata_30.rdata")
 saveRDS(simdata_20,"BN_simdata/IGF_simdata_20.rdata")
 
 #Checking pairs plots 
